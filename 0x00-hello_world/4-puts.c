2392bfcb2a975092ac77adca42518433d498ab3d
ricky-murz <mureithiericc@gmail.com>
2023-06-15 10:27:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- * Description: Prints a message using puts function
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
+
