5ffa86348595deaa54c59198d7b970e9db47867f
ricky-murz <mureithiericc@gmail.com>
2023-06-15 16:03:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,15 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to standard error
+ *
+ * Return: 1 (Error)
+ */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+	/* Writing to standard error using the write function */
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
