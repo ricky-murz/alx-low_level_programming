c64d669fb1ff92eb25443a38c7869b2a4fd06fb4
ricky-murz <mureithiericc@gmail.com>
2023-06-15 10:27:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,6 @@
-/*
- * File: 4-puts.c
- * Author: Eric Murimi
- * Date: June 15, 2023
- * Description: A program that prints the string "Programming is like building a multilingual puzzle" using the puts function.
- */
-
 #include <stdio.h>
 
-/**
- * main - Entry point for the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("Programming is like building a multilingual puzzle");
-
-	return (0);
+int main(void) {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
