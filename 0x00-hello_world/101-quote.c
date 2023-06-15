8b43c1dc6335a946f3677af92b3cc697f96e3469
ricky-murz <mureithiericc@gmail.com>
2023-06-15 15:49:17 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always returns 1
+ */
+int main(void)
+{
+    /* Print message to standard error */
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+    return 1;
+}
