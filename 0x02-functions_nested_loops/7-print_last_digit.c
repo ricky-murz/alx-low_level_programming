ee777e404b6acdf18a37a1096ef308976c9e3d1c
ricky-murz <mureithiericc@gmail.com>
2023-06-20 18:51:15 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,18 +6,16 @@
  *
  * Return: The value of the last digit
  */
+
 int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
+	last = n % 10;
+	if (last < 0)
 	{
-		n = -n;
+	last = last * -1;
 	}
-
-	last_digit = n % 10;
-
-	_putchar(last_digit + '0');
-
-	return (last_digit);
+        _putchar(last + '0');
+        return (last);
 }
