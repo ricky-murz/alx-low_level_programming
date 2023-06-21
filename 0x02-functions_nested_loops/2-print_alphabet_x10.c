2fdff4f962fd33f4c4c260b3201a87db246e4d2b
ricky-murz <mureithiericc@gmail.com>
2023-06-21 13:00:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,18 @@
  */
 void print_alphabet_x10(void)
 {
-        char c;
-        int i;
+	char c;
+	int i;
 
-        i = 0;
-        while (i < 10)
-        {
-                for (c = 'a'; c <= 'z'; c++)
-                {
-                        _putchar(c);
-                }
-                _putchar('\n');
-                i++;
-        }
+	i = 0;
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
 }
 
