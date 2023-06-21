020507c987134cd76b93f6d178ac4b88bbea8d8e
ricky-murz <mureithiericc@gmail.com>
2023-06-20 17:59:18 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,10 @@
  *
  * Return: 1 if @c is a letter, lowercase or uppercase, 0 otherwise.
  */
-int _isalpha(char c)
+
+int _isalpha(int c)
 {
+
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return 1;
