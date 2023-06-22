76280df8ceb6ae3c975a176de2e48b35cdfb10cf
ricky-murz <mureithiericc@gmail.com>
2023-06-22 09:36:16 -0700

    2-largest_number.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,20 +6,23 @@
  */
 int main(void)
 {
-        int i;
+    int i;
 
-        printf("Infinite loop incoming :(\n");
+    printf("Infinite loop incoming :(\n");
 
-        i = 0;
+    i = 0;
 
-        // Commented out the code causing the infinite loop
-        /* while (i < 10)
-        {
-                putchar(i);
-        } */
+    // Commented out the code causing the infinite loop
+    /*
+    while (i < 10)
+    {
+        putchar(i);
+        i++;
+    }
+    */
 
-        printf("Infinite loop avoided! \\o/\n");
+    printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+    return (0);
 }
 
