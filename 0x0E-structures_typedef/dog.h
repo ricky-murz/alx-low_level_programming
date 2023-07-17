d909080b753b36d6a0e5e5974ef07b95785d9f84
ricky-murz <mureithiericc@gmail.com>
2023-07-17 13:48:46 -0700

    Define struct dog with name (char*), age (float), and owner (char*)

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Pointer to a string representing the dog's name
+ * @age: Age of the dog
+ * @owner: Pointer to a string representing the dog's owner
+ */
 typedef struct dog
 {
     char *name;
     float age;
     char *owner;
 } dog_t;
+
+#endif
 
