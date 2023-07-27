16b02e25ebc83184b670720dbc6a32f582d17690
ricky-murz <mureithiericc@gmail.com>
2023-07-27 12:09:17 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,19 @@
 section .data
-    hello_msg db 'Hello, Holberton', 0xA ; Add a newline character
+    hello db 'Hello, Holberton', 0x0A ; Null-terminated string with newline
+    format db "%s", 0               ; Format string for printf
 
 section .text
-    global _start
+    global main
+    extern printf
 
-_start:
-    ; Use the write system call (syscall number 1) to print the message and newline
-    mov rax, 1            ; syscall number 1 (write)
-    mov rdi, 1            ; file descriptor 1 (stdout)
-    mov rsi, hello_msg    ; pointer to the message
-    mov rdx, 19           ; message length (number of bytes to write)
-    syscall
+main:
+    ; Prepare the arguments and call printf
+    mov rdi, format    ; Format string
+    mov rsi, hello     ; Pointer to the string to print
+    xor rax, rax       ; Clear RAX to indicate no XMM registers used for varargs
+    call printf
 
-    ; Use the exit system call (syscall number 60) to terminate the program
-    mov rax, 60           ; syscall number 60 (exit)
-    xor rdi, rdi          ; exit status 0
-    syscall
+    ; Exit the program
+    mov eax, 0         ; Return code 0
+    ret
 
---
+++ 0x12-singly_linked_lists/start.c
@@ -0,0 +1,8 @@
+/* Main file to execute the assembly program */
+void _start();
+
+int main() {
+    _start();
+    return 0;
+}
+
