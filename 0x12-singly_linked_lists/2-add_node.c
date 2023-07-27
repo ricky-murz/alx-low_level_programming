b43b88672059aaabe0dc8c29c4ab153cb53d47aa
ricky-murz <mureithiericc@gmail.com>
2023-07-27 09:34:58 -0700

    2 - Implement add_node function to prepend new node in linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,37 +3,33 @@
 #include "lists.h"
 
 /**
- * add_node - Add a new node at the beginning of a list_t list.
- * @head: Double pointer to the head of the list.
- * @str: String to be duplicated and added to the new node.
- *
- * Return: The address of the new element, or NULL if it failed.
- */
+* add_node - Adds a new node at the beginning of a linked list.
+* @head: Pointer to a pointer to the head of the linked list.
+* @str: String to be duplicated and added to the new node.
+* Return: Address of the new element, or NULL if it failed.
+*/
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
+list_t *new_node;
 
-    if (str == NULL)
-        return NULL;
+if (str == NULL)
+return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
 
-    /* Duplicate the input string using strdup */
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-    /* Set the new node's next pointer to the current head of the list */
-    new_node->next = (*head);
+new_node->len = strlen(str);
+new_node->next = *head;
+*head = new_node;
 
-    /* Update the head to point to the new node, making it the new head */
-    (*head) = new_node;
-
-    return (*head);
+return (new_node);
 }
 
