3a8bdae36cb129b11e66f409024b128b7d620ee0
ricky-murz <mureithiericc@gmail.com>
2023-07-27 09:17:06 -0700

    1 - Add list_len function to count elements in linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,21 @@
 #include <stddef.h>
+#include "lists.h"
 
 /**
- * list_len - Count the number of elements in a linked list_t list.
- * @h: A pointer to the head node of the linked list.
- *
- * Return: The number of elements in the linked list.
- */
+* list_len - Count the number of elements in a linked list_t list.
+* @h: A pointer to the head node of the linked list.
+*
+* Return: The number of elements in the linked list.
+*/
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+size_t x = 0;
 
-    while (h)
-    {
-        count++;
-        h = h->next;
-    }
-
-    return (count);
+while (h != NULL)
+{
+h = h->next;
+x++;
 }
 
+return (x);
+}
