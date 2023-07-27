e0a9c5a8f6818b4356f6166c9e6dede7d5a92071
ricky-murz <mureithiericc@gmail.com>
2023-07-27 12:55:21 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void first(void) __attribute__((constructor));
+
 /*
  * first - This function serves as a constructor
  * and is executed before main.
@@ -7,8 +9,6 @@
  * Description: This function prints the message
  * before the main function is executed
  */
-void first(void) __attribute__((constructor));
-
 void first(void)
 {
 printf("You're beat! and yet, you must allow,\n");
