27a5c19eaee557e3b0d89c40dc3065c096f573c6
ricky-murz <mureithiericc@gmail.com>
2023-08-01 16:22:57 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,31 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t list safely.
- * @h: Double pointer to the head of the list.
- * Return: The size of the list that was freed.
- */
+* struct listint_t - singly linked list
+* @value: integer
+* @next: points to the next node
+*/
+typedef struct listint_t
+{
+int value;
+struct listint_t *next;
+} listint_t;
+
+/**
+* free_listint_safe - Frees a listint_t list.
+* @h: Pointer to the head of the list.
+*
+* Return: The size of the list that was freed.
+* The function sets the head to NULL.
+*/
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t *current, *temp;
+size_t count = 0;
+listint_t *current, *next_node;
 
-	if (!h)
-		return (0);
-
-	current = *h;
-
-	while (current)
-	{
-		temp = current->next;
-		free(current);
-		current = temp;
-		count++;
-	}
-
-	*h = NULL;
-
-	return (count);
+current = *h;
+while (current)
+{
+next_node = current->next;
+free(current);
+count++;
+current = next_node;
 }
 
+*h = NULL; /* Set head to NULL after freeing the list */
+return (count);
+}
