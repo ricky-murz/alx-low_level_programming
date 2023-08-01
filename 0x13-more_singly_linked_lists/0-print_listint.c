fed0fddd35e4c76ffd90aba5775b8a0988461858
ricky-murz <mureithiericc@gmail.com>
2023-08-01 12:36:17 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
-* print_listint - Prints all elements of a listint_t list in reverse order.
+* print_listint - Prints all elements of a listint_t list.
 * @h: Pointer to the head of the list.
 *
 * Return: The number of nodes in the list.
@@ -11,16 +11,14 @@
 size_t print_listint(const listint_t *h)
 {
 size_t i = 0;
+const listint_t *current = h;
 
-/* Base case: when the list reaches the end (NULL) */
-if (h == NULL)
-return (0);
-
-/* Recursive call to print the rest of the list in reverse */
-i = print_listint(h->next);
-
-/* Print the element and increment the count */
-printf("%d\n", h->n);
-return (i + 1);
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+i++;
 }
 
+return (i);
+}
